96d0e9ab6c63f8a7cf77178f505ff50ec486a154
iMac <imac@iMacs-iMac.local>
2021-03-22 17:33:57 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/kGradientColor/kGradientColor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_kGradientColor : NSObject
+@end
+@implementation PodsDummy_kGradientColor
+@end
--- Example/kGradientColor/ViewController.swift
+++ Example/kGradientColor/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import kGradientColor
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewBg: UIView!
+    let Select = kGradientColorSet()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnSelectClick(_ sender: Any) {
+        Select.setGradientBackground(colorTop: .clear, colorBottom: .blue, view: viewBg)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
